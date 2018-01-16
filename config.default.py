3d4017ea5a7694aa562e69608d6273ee6b4c67e7
hsab <hsab@users.noreply.github.com>
2018-01-15 21:02:44 -0700

    gifs

--- config.default.py
+++
@@ -1,3 +0,0 @@
-addonsDirectory = "" # your Blender addons directory here
-skipFiles = ['config.py', 'config.default.py', 'compilation_info.json', 'setup.py', 'umog.zip']
-skipFolders = {".git", ".idea", "__pycache__", "build", "playground"}
